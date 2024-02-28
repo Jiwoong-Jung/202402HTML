098a71aac9c5e160211c8db1c1f11d371e71b965
Jiwoong-Jung <jjw981119@naver.com>
2024-02-28 11:00:39 +0900

    자바스크립트 기본

---
+++ _JS0228/constTest.js
@@ -0,0 +1,9 @@
+const myArray = [];
+myArray.push(1);
+myArray.push(2);
+myArray.push(3);
+myArray = 3;
+console.log(myArray);
+
+const x = 100;
+let y;
---
+++ _JS0228/operTest.js
@@ -0,0 +1,12 @@
+const var1 = 1024;
+const var2 = "1024";
+console.log(var1 == var2); // true
+console.log(var1 === var2); // false
+console.log(null == undefined); // true
+console.log(null == 0);
+console.log(undefined == 0);
+console.log(null === undefined);
+const id = 'admin';
+const res =(id !== null && id !== undefined) ? 
+                                '아이디 입력' : '입력 안함';
+console.log(res);
