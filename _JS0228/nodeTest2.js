55660b0547f592aadee3591a4aa6644e1cb25241
Jiwoong-Jung <jjw981119@naver.com>
2024-02-28 10:31:46 +0900

    자바스크립트 기본

---
+++ _JS0228/nodeTest2.js
@@ -0,0 +1,19 @@
+const msg = {
+    name: '홍길동',
+    age: 22,
+    addr: '서울'
+};
+
+//console.log(msg);
+
+const arr = [];   // 배열 선언
+arr.push(msg);
+//console.log(arr);
+
+const msg2 = {
+    name: '김길동',
+    age: 32,
+    addr: '인천'
+};
+arr.push(msg2);
+console.log(arr);
