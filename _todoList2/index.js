453529925ff14e6483248fd9996923a9e1a9e086
Jiwoong-Jung <jjw981119@naver.com>
2024-03-08 15:53:00 +0900

    todoList

---
+++ _todoList2/index.js
@@ -0,0 +1,10 @@
+const onClick = () => {
+    alert('Button Clicked');
+}
+
+// const onClick2 = function() {
+//     alert('Button Clicked2');
+// }
+
+document.getElementById('add-button')
+.addEventListener('click', onClick);
