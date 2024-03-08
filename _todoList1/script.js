9813b26037b7d182b73e02f313b6e5ceadbd8950
Jiwoong-Jung <jjw981119@naver.com>
2024-03-08 15:24:57 +0900

    todoList

--- _todoList1/script.js
+++ _todoList1/script.js
@@ -4,3 +4,9 @@ console.log(title1);
 
 const title2 = document.querySelector('#title');
 console.log(title2);
+
+const con1 = document.getElementsByClassName('container');
+console.log(con1);
+
+const con2 = document.querySelector('.container');
+console.log(con2);
